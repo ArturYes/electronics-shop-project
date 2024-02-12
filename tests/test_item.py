0f51a0d462f6d735fd6a3fb6a6c8456d98bb3e61
artur <youzen0you@gmail.com>
2024-02-12 22:45:06 +0600

    add tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,7 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
 
+from config import TEST_ITEM
 from src.item import Item
 
 
@@ -23,3 +24,27 @@ def test_apply_discount(product):
     product.pay_rate = 0.8
     product.apply_discount()
     assert product.price == 8000
+
+
+def test_setter(product):
+    product.name = 'iphone'
+    assert product.name == 'iphone'
+    product.name = 'iphone15maxultra'
+    assert product.name == 'iphone15ma'
+
+
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv(TEST_ITEM)
+    assert len(Item.all) == 5
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+    assert item1.price == 100
+    assert item1.quantity == 1
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
